aa2d12f7372c010d91973f0572fe269c5729ce28
BrianSamuels <itsbsamuels@gmail.com>
2023-10-11 14:30:30 -0400

    touch interaction

--- Surgery VR-AR Project/Assets/Interaction.cs
+++ Surgery VR-AR Project/Assets/Interaction.cs
@@ -13,30 +13,70 @@ public class Interaction : MonoBehaviour
     public float distanceMin = .5f;
     public float distanceMax = 15f;
     public float zoomOutMin = 2;
-    public float zoomoutMax = 20;
+    public float zoomoutMax = 43.5369f;
     public Vector3 delta = Vector3.zero;
     private Vector3 lastPos = Vector3.zero;
     float x = 0.0f;
     float y = 0.0f;
     GameObject[] array;
+    private void Awake()
+    {
+        Input.multiTouchEnabled = true;
+    }
     void Update()
     {
         // record first touch position
         if (Input.touchCount > 0)
         {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            lastPos = Input.GetTouch(0).position;
+            Debug.Log("One touch");
+            touchStart = Input.GetTouch(0).deltaPosition;
+            //touchStart = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).deltaPosition);
+            //lastPos = Input.GetTouch(0).deltaPosition;
         }
+
         // move camera
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount == 1)
         {
-            Vector3 direction = touchStart -
-            Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            Camera.main.transform.position += direction;
+            
+            //Tracks if touch is being moved on the screen
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Debug.Log("Holding Touch");
+                //Updating camera position
+                transform.position += new Vector3(touchStart.x * 10 * Time.deltaTime, 0, touchStart.y * 10 * Time.deltaTime);
+            }
         }
+
+        /*
+        if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            Debug.Log("Holding touch");
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Camera.main.transform.position += direction;
+        
+        */
+        
         // zoom
-        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        //Zoom(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.touchCount == 2)
+        {
+            Debug.Log("Two touch zoom");
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMag = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = prevMag - currentMag;
+
+            Zoom(difference * 0.01f);
+        }
+
         // rotation
+        /*
         if (Input.GetButton("Fire2"))
         {
             x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
@@ -44,26 +84,32 @@ public class Interaction : MonoBehaviour
             Quaternion rotation = Quaternion.Euler(y, x, 0);
             target.transform.rotation = rotation;
         }
+        */
         // For mobile devices, you will need to rewrite the above move, zoom and rotation
     // interactions with touch inputs.
-    //if (Input.touchCount == 2)
+    /*
+    if (Input.touchCount == 2)
         {
-            //Debug.Log("Two touches");
-            //Touch touchZero = Input.GetTouch(0);
-            //Touch touchOne = Input.GetTouch(1);
-            //Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            //Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-            //float prevMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            //float currentMag = (touchZero.position - touchOne.position).magnitude;
-            //float difference = currentMag - prevMag;
-            //Zoom(difference * 0.01f);
+            Debug.Log("Two touches");
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+            float prevMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMag = (touchZero.position - touchOne.position).magnitude;
+            float difference = currentMag - prevMag;
+            Zoom(difference * 0.01f);
         }
+    */
     }
+    
     void Zoom(float increment)
     {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize -
-        increment, zoomOutMin, zoomoutMax);
-
+        //Camera.main.orthographicSize += increment;
+        //Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, zoomOutMin, zoomoutMax);
+        Camera.main.fieldOfView += increment;
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, zoomOutMin, zoomoutMax);
+   
     }
 
 }
